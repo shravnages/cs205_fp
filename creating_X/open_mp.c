57db9903d5e6fc9064062f54989d1a82f72a46b1
Shravan Nageswaran <shravnages@gmail.com>
2020-05-08 13:15:07 -0400

    Added open_mp file

--- creating_X/open_mp.c
+++ creating_X/open_mp.c
@@ -12,16 +12,17 @@ int main (int argc, char *argv[])
 {
 int	i, j, k;
 // timing_t tstart, tend;
-// const char** corpus = {"This is the first document.",
-//     "This document is the second document.",
-//    "And this is the third one.",
-//     "Is this the first document?"};
+char corpus[][50] = {"This is the first document",
+    "This document is the second document",
+   "And this is the third one",
+    "Is this the first document"};
+
+const char* dictionary[] = {"and", "document", "first", "is", "one", "second", "the", "third", "this"};
+
+//const char** corpus = {"a b", "c a b"};
+// char corpus[][6] = {"b a", "c b a"};
 //
-// const char** dictionary = {"and", "document", "first", "is", "one", "second", "the", "third", "this"};
-
-const char** corpus = {"a b", "c a b"};
-
-const char** dictionary = {"a", "b", "c"};
+// const char *dictionary[] = {"a", "b", "c"};
 
 int c[NUM_LINES][DICT_LENGTH];
 
@@ -35,18 +36,29 @@ int c[NUM_LINES][DICT_LENGTH];
   #pragma omp parallel shared(corpus, dictionary, NUM_LINES) private(i)
   for (i=0; i<NUM_LINES; i++)
     {
-		#pragma omp for shared(DICT_LENGTH) private()
-    for(j=0; j<DICT_LENGTH; j++)
-		  {
-			char *word = strtok(corpus[i], " ");
-			char *dict_word = *(dictionary + j);
-      while (word != NULL)
-				if (strcmp(tolower(word), dict_word) == 0)
-				{
-	        c[i][j] += 1;
-				};
-				word = strtok(NULL, " ");
-		  }
+		char *word = strtok(corpus[i], " ");
+    printf("new line.....\n");
+		while (word != NULL)
+      {
+      #pragma omp for shared(DICT_LENGTH) private()
+  		for(j=0; j<DICT_LENGTH; j++)
+    		{
+    		char *dict_word = *(dictionary + j);
+        // printf("Dictword: %s\n", dict_word);
+				// printf("Word: %s\n", word);
+				if (strcmp(word, dict_word) == 0)
+				  {
+          printf("yes\n");
+          printf("%d %d \n", i, j);
+					c[i][j] += 1;
+				  }
+        else
+          {
+          printf("no\n");
+          }
+		    }
+        word = strtok(NULL, " ");
+	    }
     }
     // get_time(&tend);
 
@@ -56,8 +68,9 @@ int c[NUM_LINES][DICT_LENGTH];
 
   for (i=0; i<NUM_LINES; i++) {
       for (j=0; j<DICT_LENGTH; j++) {
-          printf(c[i][j]);
+          printf("%d, ", c[i][j]);
       }
+      printf("\n");
   }
   // printf ("Elapsed Time: %g s.\n", timespec_diff(tstart,tend));
 }
